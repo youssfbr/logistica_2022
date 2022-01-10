65e06c844a1cb2350fc705f833067527bf9644ed
Alisson Youssf <youssfbr@gmail.com>
2022-01-10 18:15:45 -0300

    Project created

---
+++ backend/src/test/java/com/github/youssfbr/logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
