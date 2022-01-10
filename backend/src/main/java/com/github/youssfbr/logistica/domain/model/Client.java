d29fe60b8bd6317fc85b494303a25830e2a0c39d
Alisson Youssf <youssfbr@gmail.com>
2022-01-10 18:37:04 -0300

    Client entity, controller test

---
+++ backend/src/main/java/com/github/youssfbr/logistica/domain/model/Client.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.logistica.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Client {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+}
